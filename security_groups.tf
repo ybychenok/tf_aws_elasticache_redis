eeca11235d77219f20417f8dc0e1677e5e163144
Yurii Bychenok <bychenok.yurii@pdffiller.team>
2019-01-24 18:18:55 +0200

    Added depends_on

--- security_groups.tf
+++ security_groups.tf
@@ -9,22 +9,23 @@ resource "aws_security_group" "redis_security_group" {
   }
 }
 
-# resource "aws_security_group_rule" "redis_ingress" {
-#   count                    = "${length(var.allowed_security_groups) > 1 ? length(var.allowed_security_groups) : 0}"
-#   type                     = "ingress"
-#   from_port                = "${var.redis_port}"
-#   to_port                  = "${var.redis_port}"
-#   protocol                 = "tcp"
-#   source_security_group_id = "${element(var.allowed_security_groups, count.index)}"
-#   security_group_id        = "${aws_security_group.redis_security_group.id}"
-# }
+resource "aws_security_group_rule" "redis_ingress" {
+  count                    = "${length(var.allowed_security_groups)}"
+  type                     = "ingress"
+  from_port                = "${var.redis_port}"
+  to_port                  = "${var.redis_port}"
+  protocol                 = "tcp"
+  source_security_group_id = "${element(var.allowed_security_groups, count.index)}"
+  security_group_id        = "${aws_security_group.redis_security_group.id}"
+  depends_on               = ["aws_security_group.redis_security_group"]
+}
 
 resource "aws_security_group_rule" "redis_networks_ingress" {
-  count             = "${length(var.custom_redis_sg) > 0 ? 0 : 1}"
   type              = "ingress"
   from_port         = "${var.redis_port}"
   to_port           = "${var.redis_port}"
   protocol          = "tcp"
   cidr_blocks       = ["${var.allowed_cidr}"]
   security_group_id = "${aws_security_group.redis_security_group.id}"
+  depends_on        = ["aws_security_group.redis_security_group"]
 }
